ee95acedd547bad961186043300039c6eaa2c7b1
수구 <@192.168.50.35>
2018-07-06 15:07:57 +0900

    [이벤트, 이미지, 유통기한] 1. 이벤트 추가하면 저장소 생성 2. 각종 이미지 파일 추가 3. 유통기한 추가 중...

--- POSMiniProject/src/model/vo/EventGroup.java
+++ POSMiniProject/src/model/vo/EventGroup.java
@@ -1,5 +1,45 @@
 package model.vo;
 
-public class EventGroup {
+import java.io.Serializable;
+import java.util.Date;
 
+public class EventGroup implements Serializable{
+
+	private String EventCode;
+	private String EventName;
+	private String EventDate;
+	private String EventType;
+	public EventGroup(String eventCode, String eventName, String eventDate, String eventType) {
+		super();
+		EventCode = eventCode;
+		EventName = eventName;
+		EventDate = eventDate;
+		EventType = eventType;
+	}
+	public String getEventCode() {
+		return EventCode;
+	}
+	public String getEventName() {
+		return EventName;
+	}
+	public String getEventDate() {
+		return EventDate;
+	}
+	public String getEventType() {
+		return EventType;
+	}
+	public void setEventCode(String eventCode) {
+		EventCode = eventCode;
+	}
+	public void setEventName(String eventName) {
+		EventName = eventName;
+	}
+	public void setEventDate(String eventDate) {
+		EventDate = eventDate;
+	}
+	public void setEventType(String eventType) {
+		EventType = eventType;
+	}
+	
+	
 }
--- POSMiniProject/src/view/totalManageSystem/popUp/GroupManage.java
+++ POSMiniProject/src/view/totalManageSystem/popUp/GroupManage.java
@@ -1,15 +1,11 @@
 package view.totalManageSystem.popUp;
 
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -17,11 +13,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
-import javax.swing.border.EmptyBorder;
 
+import dao.tmsDao.Event;
 import dao.tmsDao.Product;
+import model.vo.EventGroup;
 import model.vo.ProductGroup;
-import view.totalManageSystem.management.ManagementSystem;
 
 public class GroupManage extends JFrame {
 
@@ -112,7 +108,9 @@ public class GroupManage extends JFrame {
 					}
 				});
 			}
-		
+
+						
+				
 			
 		} else {
 			JButton modifyBtn;
