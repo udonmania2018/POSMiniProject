02739d6e2082feed7dc190994bbc15f54cce01f5
udonmania2018 <winternebel@gmail.com>
2018-07-06 19:03:35 +0900

    1. Try to change file structions
    2. 루트 디렉토리의 파일 삭제 시도

--- POSMiniProject/src/dao/tmsDao/Event.java
+++ POSMiniProject/src/dao/tmsDao/Event.java
@@ -56,7 +56,6 @@ public class Event {
 			eventdir.mkdirs();
 		}
 
-		// 파일 읽어오기
 		try (ObjectInputStream ois = new ObjectInputStream(
 				new FileInputStream(eventdir.getPath() + "\\EventGroup.dat"))) {
 
@@ -69,7 +68,7 @@ public class Event {
 			}
 
 		} catch (EOFException e) {
-			System.out.println("데이터 로드 성공");
+			System.out.println("Successfully running");
 		} catch (FileNotFoundException e) {
 
 			return null;
